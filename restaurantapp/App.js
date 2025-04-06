65accd8aaede87f89e3a7d08d6e2f78af3d7a7e3
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-04-07 01:31:45 +0300

    gerekli paketler yüklendi.

--- restaurantapp/App.js
+++ restaurantapp/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ali Can Yücel</Text>
+      <Text>Bilgisayar Mühendisi</Text>
       <StatusBar style="auto" />
     </View>
   );
