64ca79aa039cb2427754cd435e0487d4ebf0f70f
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-04-08 15:43:15 +0300

    arama eklendi...

--- restaurantapp/App.js
+++ restaurantapp/App.js
@@ -1,25 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SearchScreen from './screens/SearchScreen';
 
 const Stack = createNativeStackNavigator();
-
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerTitle: 'Ali Can Yücel - Restoran Uygulaması',
-          headerStyle: {
-            backgroundColor: '#2196F3', 
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      >
+      <Stack.Navigator screenOptions={{ headerTitle: 'Restoran Uygulaması' }}>
         <Stack.Screen name="Search" component={SearchScreen} />
       </Stack.Navigator>
     </NavigationContainer>
