cfee1ad8dd330569786447bce4e1258e11862d54
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-04-08 18:12:35 +0300

    debugger

--- restaurantapp/api/yelp.js
+++ restaurantapp/api/yelp.js
@@ -4,8 +4,9 @@ export default async function fetchYelpData() {
     'Authorization': 'Bearer aETOPuVSQ-luGy16YjLMitcWYtZh4W9pRa8Z8hk5Tz42JZA2f2UPY0ve9r7XDLnrlfVrbVqqJ-MagxP1AugW2wZgFVw2cl2ci8tq0UnFZRzUsT7_x_0-7hVa4F-RZHYx'
   };
 
-debugger;
+
   try {
+    debugger;
     const response = await fetch(url, {
       method: 'GET',
       headers: headers,
