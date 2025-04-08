bcb6ee36ecf2cdf54fce40c6278d3d11d67a2d4b
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-04-08 16:29:56 +0300

    buugn  bitecek....

--- restaurantapp/hooks/useResults.js
+++ restaurantapp/hooks/useResults.js
@@ -13,7 +13,14 @@ export default () => {
     });
     setResults(response.data.businesses);
   };
+/*
+ek bilgiler araştırmalarım useEffect, 
+React'te yan etkileri (side effects) yönetmek için kullanılan bir hook'tur.
+ React komponentleri, render işlemi sırasında doğrudan bir yan etkiye neden olmazlar
+ . Ancak, örneğin veri çekme, DOM güncelleme, zamanlayıcılar veya event listener'lar gibi işlemler gibi yan etkilerle ilgilenmek gerekebilir. 
+ useEffect, bu tür yan etkileri yönetmek için kullanılır.Yani crud gibi işlemler onclick gibi eventlerde
 
+*/ 
   useEffect(() => {
     searchApi('Toast');
   }, []);
