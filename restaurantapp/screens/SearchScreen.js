5f3dacad9fe8471a15f7130924fb0f448229add9
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-04-08 17:02:12 +0300

    axiox hatası var

--- restaurantapp/screens/SearchScreen.js
+++ restaurantapp/screens/SearchScreen.js
@@ -2,15 +2,32 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
-import ResultList from '../components/ResultList';
+import ResultsList from '../components/ResultsList';
 
 export default function SearchScreen() {
   const [searchApi, results] = useResults();
   console.log(results);
+
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
   return (
     <View>
       <SearchBar />
-      <ResultList />
+      <ResultsList
+        title="Ucuz Restoranlar"
+        results={filterResultsByPrice('₺')}
+      />
+      <ResultsList
+        title="Uygun Restoranlar"
+        results={filterResultsByPrice('₺₺')}
+      />
+      <ResultsList
+        title="Pahalı Restoranlar"
+        results={filterResultsByPrice('₺₺₺')}
+      />
     </View>
   );
 }
